0439bd27e68076634bf48da499bdb6166fb3ea62
caroline-ctrl <duon.caroline@gmail.com>
2020-07-01 12:11:44 +0200

    création du model

---
+++ models/User_model.js
@@ -0,0 +1,92 @@
+const mongoose = require('mongoose');
+
+const runningSchema = mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    pseudo: {
+        type: String,
+        required: true
+    },
+    mail: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,
+        required: false
+    },
+    is_active: {
+        type: Boolean,
+        required: true
+    }
+    // size: {
+    //     type: Number,
+    //     required: true
+    // },
+    // weight: {
+    //     weight: {
+    //         type: Number,
+    //         required: true
+    //     },
+    //     date_weight: {
+    //         type: Date,
+    //         required: true    
+    //     }
+    // },
+    // point: {
+    //     type: Number,
+    //     required: true
+    // },
+    // km: {
+    //     number_km: {
+    //         type: String,
+    //         required: true    
+    //     },
+    //     date_km: {
+    //         type: Date,
+    //         required: true    
+    //     },
+    //     total_km: {
+    //         type: Number,
+    //         required: true    
+    //     }
+    // },
+    // circuit: {
+    //     name: {
+    //         type: String,
+    //         required: true    
+    //     },
+    //     km: {
+    //         type: Number,
+    //         required: true    
+    //     },
+    //     date_circuit: {
+    //         type: Date,
+    //         required: true    
+    //     }
+    // }
+})
+
+module.exports = mongoose.model('RunningMap_model', runningSchema);
